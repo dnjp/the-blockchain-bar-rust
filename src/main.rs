a5e7ea020129a8a3074d8aca4a1c0f0c49c41470
dnjp <dnjp@posteo.org>
2022-01-23 15:37:41 -0500

    fix subcommand/argument configuration

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{AppSettings, Args, Parser, Subcommand};
+use clap::{Parser, Subcommand};
 
 const MAJOR: &str = "0";
 const MINOR: &str = "1";
@@ -7,10 +7,6 @@ const VERBAL: &str = "TX Add && Balances List";
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
-// this causes help message to be printed when run with `tbb balances list` when
-// it should just drop into the match block. however, this does print help when
-// running `tbb balances` with the missing `list` argument.
-#[clap(global_setting(AppSettings::ArgRequiredElseHelp))]
 struct Cli {
     #[clap(subcommand)]
     command: Option<Commands>,
@@ -20,47 +16,72 @@ struct Cli {
 enum Commands {
     /// Describes version
     Version {},
+
     /// Interact with balances (list...)
-    Balances(BalancesSubcommand),
-    /// Interact with txs (add...)
-    Tx(TxSubcommand),
-}
-
-#[derive(Args)]
-struct BalancesSubcommand {
     #[clap(subcommand)]
-    command: Option<BalancesCommands>,
+    Balances(BalancesCommands),
+
+    /// Interact with txs (add...)
+    #[clap(subcommand)]
+    Tx(TxCommands),
 }
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum BalancesCommands {
     /// Lists all balances
     List {},
 }
 
-#[derive(Args)]
-struct TxSubcommand {}
+#[derive(Subcommand, Debug)]
+enum TxCommands {
+    /// Adds new TX to database
+    Add {
+        /// From what account to send tokens
+        #[clap(long)]
+        from: String,
+
+        /// To what account to send tokens
+        #[clap(long)]
+        to: String,
+
+        /// How many tokens to send
+        #[clap(long)]
+        value: u64,
+
+        /// Possible values: 'reward'
+        #[clap(long)]
+        #[clap(default_value_t = String::from(""))]
+        data: String,
+    },
+}
 
 fn main() {
     let cli = Cli::parse();
-
-    // You can check for the existence of subcommands, and if found use their
-    // matches just as you would the top level app
     match &cli.command {
         Some(Commands::Version {}) => {
             println!("Version: {}.{}.{}-beta {}", MAJOR, MINOR, FIX, VERBAL);
         }
-        Some(Commands::Balances(x)) => match &x.command {
-            Some(BalancesCommands::List {}) => {
-                println!("LIST")
+        Some(Commands::Balances(balances)) => match balances {
+            BalancesCommands::List {} => {
+                println!("LIST");
             }
-            None => {}
         },
-        Some(Commands::Tx(_)) => {
-            println!("TX");
-        }
+        Some(Commands::Tx(tx)) => match tx {
+            TxCommands::Add {
+                from: _,
+                to: _,
+                value: _,
+                data: _,
+            } => {
+                println!("ADD: {:?}", tx);
+            }
+        },
         None => {}
     }
+}
 
-    // Continued program logic goes here...
+#[test]
+fn verify_app() {
+    use clap::IntoApp;
+    Cli::into_app().debug_assert()
 }
